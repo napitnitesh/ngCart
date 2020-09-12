5d0285123522e1835de3663db334dd8674827b31
nitesh <niteshnapit19990@gmail.com>
2020-09-12 22:38:34 +0530

    add on git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
+import { AdminComponent } from './admin/admin.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductsComponent } from './products/products.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'',component:UserComponent},
+  {path:'user',component:UserComponent,
+    children:[
+      {path:'',component:SignInComponent},
+      {path:'signin',component:SignInComponent},
+      {path:'signup',component:SignUpComponent}
+    ]
+  },
+
+  {path:'products',component:ProductsComponent,
+    children:[
+      {path:'cart',component:CartComponent}
+    ]},
+
+  {path:'admin',component:AdminComponent,
+    children:[
+      {path:'products',component:AdminProductsComponent},
+      {path:'users',component:AdminUsersComponent}
+    ]}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    AdminProductsComponent,
+    AdminUsersComponent,
+    ProductsComponent,
+    CartComponent,
+    UserComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
